838bd7dcea038c21c57e37fd947598a282b29343
ExploreDevelopmentSystems <mickshaymusic@gmail.com>
2024-06-07 19:08:46 -0400

    Update LibSource.lua

--- Assets/LibSource.lua
+++ Assets/LibSource.lua
@@ -1,6 +1,5 @@
 local VapeLib = {}
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ExploreDevelopmentSystems/VapeLib/main/Assets/Setup.lua"))()
--- CreateWindow Function
 function VapeLib:CreateWindow(args)
     assert(args.Title, "Title is required")
 
@@ -28,7 +27,60 @@ function VapeLib:CreateWindow(args)
 
     clonedWindow.Parent = libContainer
 
+    -- Add Tabs container if not present
+    if not clonedWindow:FindFirstChild("Tabs") then
+        local Tabs = Instance.new("Folder")
+        Tabs.Name = "Tabs"
+        Tabs.Parent = clonedWindow
+    end
+
     return clonedWindow
+end
+
+-- CreateTab Function
+function VapeLib:CreateTab(window, args)
+    assert(window, "Window is required")
+    assert(args.Name, "Name is required")
+    assert(args.Image, "Image asset ID is required")
+
+    local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+    -- Ensure unique identifiers for tabs
+    local tabName = args.Name
+    if window:FindFirstChild(tabName) then
+        error("A tab with this name already exists in the window.")
+    end
+
+    -- Clone and setup TabButton
+    local tabButtonTemplate = ReplicatedStorage:WaitForChild("Asset"):WaitForChild("Window"):WaitForChild("TabButton")
+    local clonedTabButton = tabButtonTemplate:Clone()
+
+    local textLabel = clonedTabButton:FindFirstChild("TextLabel")
+    if textLabel then
+        textLabel.Text = args.Name
+    end
+
+    local imageLabel = clonedTabButton:FindFirstChild("ImageLabel")
+    if imageLabel then
+        imageLabel.Image = "rbxassetid://" .. tostring(args.Image)
+    end
+
+    clonedTabButton.Name = tabName
+    clonedTabButton.Parent = window:FindFirstChild("Tabs")
+
+    -- Clone and setup corresponding window frame
+    local windowFrameTemplate = ReplicatedStorage:WaitForChild("Asset"):WaitForChild("Window"):WaitForChild("Window")
+    local clonedWindowFrame = windowFrameTemplate:Clone()
+
+    local windowNameLabel = clonedWindowFrame:FindFirstChild("WindowName") or clonedWindowFrame:FindFirstChild("TextLabel")
+    if windowNameLabel then
+        windowNameLabel.Text = args.Name
+    end
+
+    clonedWindowFrame.Name = tabName
+    clonedWindowFrame.Parent = window
+
+    return clonedTabButton, clonedWindowFrame
 end
 
 return VapeLib
