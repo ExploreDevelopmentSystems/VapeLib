63e3c25ad299159cfe62c2b652eb1b5160c4dfa8
ExploreDevelopmentSystems <mickshaymusic@gmail.com>
2024-06-08 02:04:44 -0400

    Update LibSource.lua

--- Assets/LibSource.lua
+++ Assets/LibSource.lua
@@ -155,14 +155,21 @@ function VapeLib:CreateModule(tab, moduleArgs)
     local ReplicatedStorage = game:GetService("ReplicatedStorage")
     local moduleIdentifier = generateRandomString(6)
 
+    -- Validate tab existence in VapeLib.Tabs
+    if not VapeLib.Tabs[tab.Name] then
+        error("Tab not found in VapeLib.Tabs")
+    end
+
     local assetFolder = findChildOrError(ReplicatedStorage, "Asset", "ReplicatedStorage")
     local moduleFolder = findChildOrError(assetFolder, "Module", "ReplicatedStorage.Asset")
     local moduleTemplate = findChildOrError(moduleFolder, "Module", "ReplicatedStorage.Asset.Module")
     local moduleFrameTemplate = findChildOrError(moduleFolder, "ModuleFrame", "ReplicatedStorage.Asset.Module")
 
+    -- Clone the module template and set its name
     local clonedModule = moduleTemplate:Clone()
     clonedModule.Name = moduleIdentifier
 
+    -- Set the module name
     local textLabel = clonedModule:FindFirstChild("TextLabel")
     if textLabel then
         textLabel.Text = moduleArgs.Name
@@ -170,21 +177,26 @@ function VapeLib:CreateModule(tab, moduleArgs)
         warn("TextLabel not found in Module.")
     end
 
-    local contentFrame = VapeLib.Tabs[tab.Name].Window
+    -- Get the content frame of the tab
+    local contentFrame = VapeLib.Ttabs[tab.Name].Window
     local tabList = contentFrame:FindFirstChild("List")
     if not tabList then
         error("List frame not found in the tab's content frame.")
     end
 
+    -- Parent the cloned module to the tab list
     clonedModule.Parent = tabList
 
+    -- Clone the module frame and set its name and parent
     local clonedModuleFrame = moduleFrameTemplate:Clone()
     clonedModuleFrame.Name = moduleIdentifier
     clonedModuleFrame.Parent = tabList
     clonedModuleFrame.Visible = false
 
+    -- Add the cloned module and its frame to the tab's Modules list
     table.insert(VapeLib.Tabs[tab.Name].Modules, { Module = clonedModule, Frame = clonedModuleFrame })
 
+    -- Add callback functionality
     local moduleButton = clonedModule:FindFirstChild("TextButton")
     if moduleButton then
         moduleButton.MouseButton1Click:Connect(function()
@@ -198,5 +210,6 @@ function VapeLib:CreateModule(tab, moduleArgs)
 
     return clonedModule, clonedModuleFrame
 end
+
 
 return VapeLib
